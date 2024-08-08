41a89268fdd6dd56117ef6698262608d32c52f8c
JannisErhard <jannis.erhard@gmx.de>
2024-08-08 12:47:23 -0700

    another clustertool

---
+++ check_status.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for i in */*.out
+do 
+  job=`echo $i | sed "s/-/ /g" | sed "s/\.out//g" | awk '{print $NF}'`
+  IS=`echo $i | sed "s/\_/\ /g" | sed "s/\//\ /g" | awk '{print $1, $2, $3}'`
+  MEM=`seff $job | grep "State"`
+  echo $IS $MEM
+done  | sort -rgk 6
