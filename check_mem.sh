a25044d8fbd26463ad819e3f94b7d34717a8b9f7
JannisErhard <jannis.erhard@gmx.de>
2024-08-12 13:04:13 -0700

    cleaned up some trash

--- check_mem.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-for i in */*.out
-do 
-  job=`echo $i | sed "s/-/ /g" | sed "s/\.out//g" | awk '{print $NF}'`
-  IS=`echo $i | sed "s/\_/\ /g" | sed "s/\//\ /g" | awk '{print $1, $2, $3}'`
-  MEM=`seff $job | grep "Memory Utilized"`
-  echo $IS $MEM
-done  | sort -rgk 6
--- update_time.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sed "s/03:00:00/06:00:00/g" run_dice.sh  > tmp
-mv tmp  run_dice.sh 
