4ba753493dc17995e044a105ef8d20c0bb47da04
JannisErhard <jannis.erhard@gmx.de>
2024-08-12 13:01:34 -0700

    upadeted some scripts

--- cluster_bash_tools/update_all_timeouts.sh
+++ cluster_bash_tools/update_all_timeouts.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-for i in `sh ~/HCI_Scripts/check_status.sh  | grep TIME  | awk '{printf "%s_%s_%s\n", $1, $2, $3}'`
+for i in `sh ~/HCI_Scripts/cluster_bash_tools/check_status.sh  | grep TIME  | awk '{printf "%s_%s_%s\n", $1, $2, $3}'`
 do
   echo $i
   cd $i
-  sh ~/HCI_Scripts/update_time.sh
+  if [ $1 == 1 ]
+  then
+    sh ~/HCI_Scripts/cluster_bash_tools/update_time.sh
+  elif [ $1 == 3 ]
+  then
+    sh ~/HCI_Scripts/cluster_bash_tools/third_update_time.sh
+  fi
   sbatch run_dice.sh
   cd -
 done
