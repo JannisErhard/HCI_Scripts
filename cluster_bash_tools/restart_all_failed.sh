ba6963113a2abd132dab90e5de62aea511de4f78
JannisErhard <jannis.erhard@gmx.de>
2024-08-19 19:42:27 +0200

    bugfix, minor

--- cluster_bash_tools/restart_all_failed.sh
+++ cluster_bash_tools/restart_all_failed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in `sh ~/HCI_Scripts/cluster_bash_tools/check_status.sh  | grep Failed  | awk '{printf "%s_%s_%s\n", $1, $2, $3}'`
+for i in `sh ~/HCI_Scripts/cluster_bash_tools/check_status.sh  | grep FAILED | awk '{printf "%s_%s_%s\n", $1, $2, $3}'`
 do
   cd $i
     sbatch run_dice.sh
