e3fb3bac2b722a7b3ab8684af92b046dc63e181a
JannisErhard <jannis.erhard@gmx.de>
2024-09-07 20:12:11 -0700

    made check mem more versatile

--- cluster_bash_tools/check_mem.sh
+++ cluster_bash_tools/check_mem.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-for i in */*.out
+for j in */
 do 
-  job=`echo $i | sed "s/-/ /g" | sed "s/\.out//g" | awk '{print $NF}'`
-  IS=`echo $i | sed "s/\_/\ /g" | sed "s/\//\ /g" | awk '{print $1, $2, $3}'`
+  i=`ls $j/*.out | tail -n 1`
+  job=`echo $i | sed "s/-/ /g" | sed "s/\.out//g" | tail -n 1 | awk '{print $NF}'`
+  IS=`echo $i | sed "s/\_/\ /g" | sed "s/\//\ /g" | tail -n 1 | awk '{print $1, $2, $3}'`
   MEM=`seff $job | grep "Memory Utilized"`
   echo $IS $MEM
 done  | sort -rgk 6
